fd781d70fb7dff25fb0868b01416eb1ac8032b32
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2025-09-20 18:15:42 +0700

    “update

--- app/main/views.py
+++ app/main/views.py
@@ -3,17 +3,27 @@ from datetime import datetime
 from flask import render_template, session, redirect, url_for, request, flash
 from . import main
 from .forms import NameForm
+from datetime import datetime
+from .. import db
+from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    comments = ["Nice app!", "Great work!"]  # مثال ثابت
+    comments = ["Nice app!", "Great work!"]
 
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
+        name = form.name.data
+        user = User.query.filter_by(username=name).first()
+        if user is None:
+            user = User(username=name)
+            db.session.add(user)
+            db.session.commit()
+            flash("New user added to database!")
+        else:
+            flash("Welcome back!")
+
+        session['name'] = name
         return redirect(url_for('.index'))
 
     return render_template(
@@ -24,6 +34,8 @@ def index():
         form=form,
         comments=comments
     )
+    
+    
 
 # صفحة مستخدم /user/<name>
 @main.route('/user/<name>')
